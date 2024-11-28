71577e220246caf0cb98be56f2910b3f758128e6
Lauren Li <laurenli045@gmail.com>
2024-11-28 10:05:51 -0500

    added player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,15 @@ public class Player : MonoBehaviour
     public float speed = 1.5f;
     private LevelManager levelManager;
     private GameObject item;
+    [SerializeField] 
+    [Tooltip("Insert Animator Controller")]
+    private Animator playerAnimator;
     
     // Start is called before the first frame update
     void Start()
     {
         levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        playerAnimator.SetBool("isWalking", false);
     }
 
     // Update is called once per frame
@@ -20,19 +24,27 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a")) {
             transform.position += Vector3.left * speed * Time.deltaTime;
+            playerAnimator.SetBool("isWalking", true);
         }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+            playerAnimator.SetBool("isWalking", true);
         }
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
             transform.position += Vector3.up * speed * Time.deltaTime;
+            playerAnimator.SetBool("isWalking", true);
         }
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
             transform.position += Vector3.down * speed * Time.deltaTime;
-        } 
+            playerAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalking", false);
+        }
         
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         if (item != null)
